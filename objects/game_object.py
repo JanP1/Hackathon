32022d36eb250884c30b13e1ec00528c81a7f30b
MrRooby <mrrooby64@gmail.com>
2025-11-29 12:35:04 +0100

    added game_object base class

---
+++ objects/game_object.py
@@ -0,0 +1,59 @@
+import pygame
+from abc import ABC, abstractmethod, abstractclassmethod
+
+'''
+Podstawowy game object po którym będzie dziedziczyło wszystko co może poruszać 
+się po ekranie.
+'''
+class GameObject(ABC):
+    def __init__(self, x_pos: int, y_pos:int, SCREEN_W: int, SCREEN_H: int, name: str = "object"):
+        self.sprite = pygame.image.load('assets/pictures/default_sprite.png')
+        self.sprite_flipped = pygame.transform.flip(self.sprite, True, False);
+
+        self.rect = self.sprite.get_rect() # !!! musimy ustalić jak robimy z hitboxem, czy zawsze wielkości spritea 
+        
+        self.speed = 1
+        
+        self.SCREEN_W = SCREEN_W
+        self.SCREEN_H = SCREEN_H
+
+        self.rect.x = x_pos
+        self.rect.y = y_pos
+
+        self.sounds = {}
+        self.sprites = {}
+
+        self.name = name
+
+        self.facing_right = True
+
+
+    def init_sounds(self, sound_dict):
+        for sound_name, sound_path in sound_dict.items():
+            self.sounds[sound_name] = pygame.mixer.Sound(sound_path)
+
+
+    def init_sprites(self, sprite_dict):
+        for sprite_name, sprite_path in sprite_dict.items():
+            self.sprites[sprite_name] = pygame.image.load(sprite_path)
+
+
+    def play_sound(self, sound_name):
+        if sound_name in self.sounds:
+            self.sounds[sound_name].play()        
+
+
+    def set_sprite(self, sprite_name):
+        if sprite_name in self.sprites:
+            self.sprite = self.sprites[sprite_name]
+            self.sprite_flipped = pygame.transform.flip(self.sprite, True, False) 
+
+
+    def draw(self, screen):
+        current_sprite = self.sprite if self.facing_right else self.sprite_flipped
+        screen.blit(current_sprite, self.rect)
+
+
+    @abstractmethod
+    def update(self):
+        print(self.name + " update called")

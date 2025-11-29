1fa2fe482488713649adaf2b2c1517c8f7fec743
JanP1 <janp1krc@gmail.com>
2025-11-29 20:32:23 +0100

    Dzialajacy ruch po mapie i animacja

--- TestLevel/test_level.py
+++ TestLevel/test_level.py
@@ -1,22 +1,77 @@
 import pygame
 import sys
+from objects.player import Player
+from objects.camera import Camera
+
+WIDTH = 1920
+HEIGHT = 1080
 
 class TestLevel:
-    def __init__(self, display, game_state_manager) -> None:
+    def __init__(self, display, game_state_manager):
         self.display = display
         self.game_state_manager = game_state_manager
 
+        # load background
+        self.bg = pygame.image.load("assets/pictures/backgrounds/main_background.png")
+        self.bg_width = self.bg.get_width()
+        self.bg_height = self.bg.get_height()
+
+        # spawn player near center
+        self.player = Player(WIDTH//2, HEIGHT//2,
+                            WIDTH,
+                            HEIGHT,
+                              4096, # map width
+                              4096  # map height
+                              )
+
+        # camera with smaller box (800x600)
+        self.camera = Camera(self.bg_width, self.bg_height, WIDTH, HEIGHT, box_w=800, box_h=600)
+
     def run(self):
-        self.display.fill("red")
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.game_state_manager.set_state("start")
+                pygame.quit(); sys.exit()
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+                self.game_state_manager.set_state("start")
+
+        # update player
+        keys = pygame.key.get_pressed()
+        self.player.update(keys)
+        
+
+        # update camera
+        self.camera.update(self.player)
+        
+
+        # draw
+        self.display.blit(self.bg, (-self.camera.x, -self.camera.y))
+        #self.display.blit(self.player.sprite, self.camera.apply(self.player.rect))
 
 
-        # jak wywoływać i rysować elementy 
-        # game_object.update()
-        # game_object.draw(self.display)
+        # get camera-adjusted rect
+        sprite_rect = self.camera.apply(self.player.rect).copy()
+
+
+
+        # ============================================================
+        # Tutaj jest offset zeby hitbox byl w srodku
+
+        # apply offset to center the PNG on the hitbox
+        image_offset = pygame.Vector2(-35, -55)  # tweak these values
+        sprite_rect.x += image_offset.x
+        sprite_rect.y += image_offset.y
+        
+        # ============================================================
+        
+        # draw the sprite
+        self.display.blit(self.player.sprite, sprite_rect)
+
+
+        # checking hitbox
+        # draw hitbox rectangle (always red, thickness=2)
+        pygame.draw.rect(
+            self.display,
+            (255, 0, 0),
+            self.camera.apply(self.player.rect),  # apply camera offset
+            2
+        )
